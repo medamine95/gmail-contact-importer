6fd2e1767d19acf3da9bc051556155e45d4b00fb
medamine95 <benbelgacem.mohamed@live.com>
2019-03-01 19:23:21 +0100

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="css/app.css">
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div id="example"></div>
+
+    <script  type="text/javascript" src="js/app.js"> </script>
+    </body>
+</html>
